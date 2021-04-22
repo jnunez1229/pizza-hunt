52a4e6df479dc3a4c43ab2571385a2f8c8bbc442
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-04-22 16:43:59 -0500

    deployed on heroku

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhostepizza-hunt',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhostpizza-hunt',{
     useFindAndModify: false,
     useNewURLParser: true,
     useUnifiedTopology: true
