2af576250b287c44192ab4820644f0c575e72da2
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-04-22 13:03:34 -0500

    added reply logic to comments

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,57 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model, Types} = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-    writtenBy: {
-        type: String
+const ReplySchema = new Schema(
+    {
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
     },
-    commentBody: {
-        type: String
-    },
-    createdAT: {
-        type: Date,
-        default: Date.now
+    {
+        toJSON: {
+            getters: true
+        }
     }
-});
+);
 
+const CommentSchema = new Schema(
+    {
+        writtenBy: {
+            type: String
+        },
+        commentBody: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+CommentSchema.virtual('replyCount').get(function(){
+    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+})
 const Comment = model('Comment', CommentSchema);
 
 module.exports = Comment;
